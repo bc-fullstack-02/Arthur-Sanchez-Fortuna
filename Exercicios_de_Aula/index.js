edcf033c8f9bd99a612f522a7990defcdb545135
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-11-16 03:45:49 -0300

    Meu cerebro ta a 1 por hora

--- Exercicios_de_Aula/index.js
+++ Exercicios_de_Aula/index.js
@@ -21,8 +21,68 @@ const db = [
     {id: 'a460767c-6568-11ed-9022-0242ac120004', title: 'Salve DB4', body: 'Oi meu chapa, o sr goxta de pasçoca?', create_at: new Date()}
 ]
 
-/*TD EXTA KRLHA É 1 CRUD*/
+router
+    .route('/posts')
+    .all((req, res, next) =>{
+        console.log(new Date())
+        next()
+    })
+    .get((req, res) => {
+        res.send(db)
+    })
+    .post((req, res) => {
+        db.push(req.body);
+        res.status(201);
+        res.end();
+    })
 
+router.param('id', (req, res,next, id) => {
+        console.log(req.body)
+        console.log(new Date())
+        next()
+    })
+    .route('/posts/:id')
+    .get((req, res) => {
+        const ret = db.find(e => e.id === req.params.id);
+        if(ret) {
+            res.send(ret);
+        }else {
+            res.status(404).end();
+        }
+    })
+    .put((req, res) => {
+        const ret = db.find(e => e.id === req.params.id);
+        if (ret) {
+            db = db.map((e) => {
+                if(e.id === req.params.id) {
+                    return req.body
+                } else {
+                    return
+                }
+            })
+            res.status(202)
+            res.end()
+        } else {
+            res.status(404).end()
+        }
+    })
+    .delete((req, res) => {
+        const ret = db.find(e => e.id === req.params.id);
+        if (ret) {
+            db = db.filter(e => e.id !== req.params.id);
+            res.status(204);
+            res.end();
+        } else {
+            res.status(404).end();
+        }
+    })
+
+app.use(router);
+
+app.listen(3000);
+console.log ('server rodando na porta 3k')
+
+/*TD EXTA KRLHA É 1 CRUD
 app.get('/posts', (req, res) => {
     res.send(db)
 })
@@ -71,6 +131,4 @@ app.delete('/posts/:id', (req, res) => {
         res.status(404).end()
     }
 })
-
-app.listen(3000);
-console.log ('server rodando na porta 3k')
+*/
