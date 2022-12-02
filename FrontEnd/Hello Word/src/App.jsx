f2e6773db86bcd93e5c0852d443ff7d3e20850ae
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-12-02 05:59:34 -0300

    Hello Word Tá Funfando

---
+++ FrontEnd/Hello Word/src/App.jsx
@@ -0,0 +1,15 @@
+import Contador from "./Contador";
+
+function App() {
+
+  return (
+      <div className="App">
+        <h1>SALVE MEU POVO</h1>
+        <h2>Meu 1° Codigo de React</h2>
+        <h3>Tá Contando</h3>
+        <Contador />
+      </div>
+  );
+}
+
+export default App;
