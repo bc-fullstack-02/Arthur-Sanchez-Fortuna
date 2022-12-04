77bb621b9f27a6de3549115391e9455e5855ad47
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-12-04 09:46:45 -0300

    Até o fundo preto foi

---
+++ FrontEnd/Web/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /*O ocnteudo q ele vai ler */
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  /*Fazer rodar, minuto 48:50 Aula 4*/
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/*https://tailwindcss.com/docs/installation/using-postcss ===> PRA INSTALAR*/
+/*Tailwind CSS IntelliSense ==> Extensão */
