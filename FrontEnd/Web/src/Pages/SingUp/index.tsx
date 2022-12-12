2164f3c615bc66bfd9e68f6b779a5064e0a3d17a
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-12-12 11:32:33 -0300

    Fim da Aula 6

---
+++ FrontEnd/Web/src/Pages/SingUp/index.tsx
@@ -0,0 +1,18 @@
+// Aula 6 min 26:30
+
+import { AuthForm } from "../../Components/AuthForm";
+
+export function SingUp() {
+    function handleRegister(user: string, password: string){
+
+    }
+
+    return (
+        <AuthForm
+            formTitle="Faça seu Cadastro para Entrar!"
+            submitFormButtonText="Cadastrar"
+            submitFormButtonAction={handleRegister}
+            linkDescription="Já Tem Uma Conta? Entre Agora!"
+            routeName="/"/>
+    );
+};
