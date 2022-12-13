689ae58f40d073500f477262e66f3699d7c69207
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-12-13 19:21:06 -0300

    A Volta Dos Que Não Foram

--- FrontEnd/Web/src/Pages/Login/index.tsx
+++ FrontEnd/Web/src/Pages/Login/index.tsx
@@ -1,17 +1,36 @@
+// Aula 7 min 18,, npm install jwt-decode
+import jwt_decode from 'jwt-decode';
+//  biblioteca pequena que é só pra pegar o token e pegaroq tiver dentro dele
+
 // Explicação das Rotas Aula 6 min 28 até 35:25, npm instal react-router-dom
 import { AuthForm } from "../../Components/AuthForm";
 import api from "../../Services/api";
+
+interface UserToken {
+    profile: string;
+    user: string;
+};
 
 // Explicação Aula 6 min 24 até 26:20
 export function Login() {
 // aula 6 min 1h e 11 até 1h e 16
     async function handleLogin(user:string, password: string) {
-        const data = await api.post("/security/login", {
-            user,
-            password
-        });
-        console.log(data)
-    }
+        // aula 7 min 22 até 28
+        try {
+            const { data } = await api.post("/security/login", {
+                user,
+                password
+            });
+            const decodeToken = jwt_decode(data.accessToken) as UserToken;
+            localStorage.setItem("profile", decodeToken.profile);
+            localStorage.setItem("user", decodeToken.user);
+            localStorage.setItem("accessToken", data.accessToken);
+        }
+        catch (err) {
+            console.log(err);
+            alert("Ocorreu um Erro no Login");
+        }
+    };
 
     return (
         <AuthForm
